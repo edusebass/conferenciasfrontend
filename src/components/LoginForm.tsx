0287350866ba403bc5938d0241f044c858329dcb
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-03-25 08:18:09 -0500

    loginform

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,55 @@
+"use client"
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+import { useAuth } from '@/context/AuthContext';
+import { Button } from 'antd';
+
+export default function LoginForm() {
+    const router = useRouter();
+    const { login } = useAuth();
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+
+    const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const formData = new FormData(event.currentTarget);
+        const email = formData.get('email') as string;
+        const password = formData.get('password') as string;
+
+        try {
+            await login(email, password);
+            setLoading(true)
+        } catch (error:any) {
+            console.log(error)
+            setLoading(false)
+        }
+    };
+
+    return (
+        <section className="bg-gray-50 ring-1 rounded-3xl">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto">
+                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                            Inicia Sesión
+                        </h1>
+                        <form className="space-y-4 md:space-y-6" onSubmit={handleLogin}>
+                            <div>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 ">Email</label>
+                                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
+                            </div>
+                            <div>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contraseña</label>
+                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                            </div>
+
+                            <Button type="primary" htmlType='submit' loading={loading} className={`w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5  text-center ${loading ? 'opacity-50 cursor-not-allowed' : ''}`} disabled={loading}>Ingresar</Button>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
